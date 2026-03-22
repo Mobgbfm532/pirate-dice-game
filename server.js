4a01dc411b1bd508c35cbab09a638689f21d43db
Mobgbfm532 <54285530+Mobgbfm532@users.noreply.github.com>
2026-03-22 20:21:52 +0000

    Update server.js

--- server.js
+++ server.js
@@ -148,8 +148,9 @@ io.on('connection', (socket) => {
             room.roundPlayers = room.roundPlayers.map(id => id === existingPlayerId ? socket.id : id);
             
         } else {
+            // UPDATED: Players now start with 10 lives for testing
             room.players[socket.id] = { 
-                id: socket.id, token: data.token, name: finalName, avatar: data.avatar, lives: 2, score: null, busted: false, connected: true 
+                id: socket.id, token: data.token, name: finalName, avatar: data.avatar, lives: 10, score: null, busted: false, connected: true 
             };
             room.playerOrder.push(socket.id);
 
@@ -183,8 +184,6 @@ io.on('connection', (socket) => {
     socket.on('playerRolledDice', (suspenseType) => socket.to(socket.roomCode).emit('playerRolledDice', suspenseType));
     socket.on('playGameSound', (soundId) => socket.to(socket.roomCode).emit('playGameSound', soundId));
     socket.on('triggerConfetti', () => socket.to(socket.roomCode).emit('triggerConfetti'));
-    
-    // UPDATED: Video removed, now triggers the 2-second visual effect
     socket.on('triggerDevilEffect', () => socket.to(socket.roomCode).emit('triggerDevilEffect'));
     
     socket.on('updateBoard', (gameData) => {
@@ -204,7 +203,8 @@ io.on('connection', (socket) => {
             room.players[socket.id].score = turnData.score;
             room.players[socket.id].busted = turnData.busted;
             
-            if (turnData.score === 24 && room.players[socket.id].lives < 2) {
+            // UPDATED: 24 now restores a life up to a maximum of 10
+            if (turnData.score === 24 && room.players[socket.id].lives < 10) {
                 room.players[socket.id].lives += 1;
                 io.to(socket.roomCode).emit('displayMessage', { text: `✨ +1 Life Restored! ✨`, color: "#aed581" });
             }
